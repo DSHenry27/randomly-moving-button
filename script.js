567dec0f323e5d99850d97708778e5443c4aa0ad
Nguyễn Hữu Luân <163289169+DSHenry27@users.noreply.github.com>
2024-12-16 08:59:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "😆😆🫣🫣😆😆😘";
+  question.innerHTML = "Bạn chọn đúng rồi 😆";
   gif.src =
     "https://media1.giphy.com/media/iCVzZwwE6QNAV2tEE0/giphy.gif";
 });
